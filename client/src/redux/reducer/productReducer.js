4e8d34605db5e2abeefc999f9b89b777ab7583d1
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-08-16 16:15:40 -0300

    feat (redux/reducer) add reducer functions

--- client/src/redux/reducer/productReducer.js
+++ client/src/redux/reducer/productReducer.js
@@ -1,7 +1,22 @@
-const initialState = {};
+import {GET_ALL_PRODUCTS, GET_PRODUCT} from "../../utils/reduxVars.js"
+
+const initialState = {
+  products:[],
+  productDetail:{}
+};
 
 export function productReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return{
+        ...state,
+        products:action.payload
+      }
+      case GET_PRODUCT:
+        return{
+          ...state,
+          productDetail:action.payload
+        }
     default:
       return state;
   }
