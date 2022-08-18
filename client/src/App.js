be71c1c543912fdbc52d176f7ea5cecebc3d8dea
Alejo Ufano <alejiiiinn@gmail.com>
2022-08-18 16:48:25 -0300

    Feat(components/pages/Home) Add Provisory Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import "./App.css";
+import './App.css';
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import Header from "./components/organisms/Header/Header";
+import Header from './components/organisms/Header/Header';
+import Home from './components/pages/Home/Home.jsx';
 
 function App() {
   return (
-    <>
-      <Header />
-
-      <Routes>{/* aca irian las <Route /> */}</Routes>
+    <>      
+      <Routes>
+        <Route path='/' element={<Home/>}/>                
+      </Routes>
     </>
   );
 }
