905a06b9ca746dedce2e5bd5da56d2ace6121d74
Alejo Ufano <alejiiiinn@gmail.com>
2022-08-19 16:57:44 -0300

    Feat(components/ card-home) Card Home

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import Carousel from 'react-material-ui-carousel'
 import Card from "../../organisms/Card/Card";
 import { getAllProducts } from "../../../redux/actions/productActions.js";
 import style from "./Home.module.css";
@@ -12,11 +12,26 @@ let Home = () => {
   useEffect(() => {
     dispatch(getAllProducts());
   }, []);
+  
   return (
-    <div className={style.container}>
-      {products.map((product) => (
-        <Card product={product} />
-      ))}
+    <div className={style.globalContainer}>
+        <div className={style.carouselContainer}>
+        {/* <Carousel>
+            {
+                items.map( (item) => <div>{item.description}</div> )
+            }
+        </Carousel>         */}
+        </div>
+        <div className={style.functionalitiesContainer}>
+            <div className={style.utilities}>
+                
+            </div>
+            <div className={style.cardsContainer}>
+              {products.map((product) => (
+                <Card product={product} />
+              ))}
+            </div>
+        </div>
     </div>
   );
 };
