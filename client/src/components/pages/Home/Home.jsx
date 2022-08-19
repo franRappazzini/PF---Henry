f648acfdf3eafd9d64c0cb0f231c63456853275d
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-19 17:47:04 -0300

    feat (client/Home): create pagination

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,37 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Carousel from 'react-material-ui-carousel'
+
 import Card from "../../organisms/Card/Card";
+import Carousel from "react-material-ui-carousel";
+import { Pagination } from "@mui/material";
 import { getAllProducts } from "../../../redux/actions/productActions.js";
 import style from "./Home.module.css";
 
 let Home = () => {
   let { products } = useSelector((state) => state.product);
   let dispatch = useDispatch();
+  const [page, setPage] = useState(1);
+  const prodPerPage = 12;
+  const totalPage = Math.ceil(products.length / prodPerPage);
 
   useEffect(() => {
     dispatch(getAllProducts());
-  }, []);
-  
+  }, [dispatch]);
+
   return (
     <div className={style.globalContainer}>
-        <div className={style.carouselContainer}>
+      <div className={style.carouselContainer}>
         {/* <Carousel>
             {
                 items.map( (item) => <div>{item.description}</div> )
             }
         </Carousel>         */}
+      </div>
+      <div className={style.functionalitiesContainer}>
+        <div className={style.utilities}></div>
+        <div className={style.cardsContainer}>
+          {products
+            .slice(
+              (page - 1) * prodPerPage,
+              (page - 1) * prodPerPage + prodPerPage
+            )
+            .map((product) => (
+              <Card product={product} />
+            ))}
         </div>
-        <div className={style.functionalitiesContainer}>
-            <div className={style.utilities}>
-                
-            </div>
-            <div className={style.cardsContainer}>
-              {products.map((product) => (
-                <Card product={product} />
-              ))}
-            </div>
-        </div>
+      </div>
+
+      <section className={style.pagination_container}>
+        <Pagination
+          count={totalPage}
+          shape="rounded"
+          onChange={(e, value) => setPage(value)}
+        />
+      </section>
     </div>
   );
 };
