3d60a750a94040014dbeb07f9d50b5b0f8d31999
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-19 12:45:28 -0300

    fix (client and server): GET /:id and POST form

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,11 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store/store";
 
 // TODO configurar theme
 const theme = createTheme({
@@ -28,11 +30,13 @@ const theme = createTheme({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,11 +1,19 @@
-import { GET_ALL_PRODUCTS, GET_PRODUCT,GET_PRODUCT_NAME,FILTER_BY_BRAND,FILTER_BY_CATEGORY,FILTER_BY_SIZE,FILTER_BY_PRICE} from "../../utils/reduxVars";
+import {
+  FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+  FILTER_BY_SIZE,
+  GET_ALL_PRODUCTS,
+  GET_PRODUCT,
+  GET_PRODUCT_NAME,
+} from "../../utils/reduxVars";
 
 import axios from "axios";
 
 export function getAllProducts() {
   return async (dispatch) => {
     try {
-      const res = await axios.get("http://localhost:3001/products");
+      const res = await axios.get("http://localhost:3001/product");
       dispatch({ type: GET_ALL_PRODUCTS, payload: res.data });
     } catch (err) {
       console.log(err);
@@ -16,7 +24,7 @@ export function getAllProducts() {
 export function getProduct(id) {
   return async (dispatch) => {
     try {
-      const res = await axios.get(`http://localhost:3001/products/${id}`);
+      const res = await axios.get(`http://localhost:3001/product/${id}`);
       dispatch({ type: GET_PRODUCT, payload: res.data });
     } catch (err) {
       console.log(err);
@@ -26,49 +34,48 @@ export function getProduct(id) {
 
 export async function createProduct(product) {
   try {
-    await axios.post("http://localhost:3001/products", product);
+    await axios.post("http://localhost:3001/product", product);
   } catch (err) {
     return err;
   }
 }
 
-
-export  function searchProduct(name){
-  return async function(dispatch){
-  try { 
-      var json = await axios.get(`http://localhost:3001/products?name=${name}`)
+export function searchProduct(name) {
+  return async function (dispatch) {
+    try {
+      var json = await axios.get(`http://localhost:3001/product?name=${name}`);
       return dispatch({
-        type:GET_PRODUCT_NAME,
-        payload:json.data
-      })
-  } catch (err) {
-    return err
-  }
-}
+        type: GET_PRODUCT_NAME,
+        payload: json.data,
+      });
+    } catch (err) {
+      return err;
+    }
+  };
 }
 
 // filtrados (por marca, categoría y talles)
-export function filterProductByBrand(payload){
-  return{
-    type:FILTER_BY_BRAND,
-    payload
-  }
+export function filterProductByBrand(payload) {
+  return {
+    type: FILTER_BY_BRAND,
+    payload,
+  };
 }
-export function filterProductByCategory(payload){
-  return{
+export function filterProductByCategory(payload) {
+  return {
     type: FILTER_BY_CATEGORY,
-    payload
-  }
+    payload,
+  };
 }
-export function filterProductBySize(payload){
-  return{
+export function filterProductBySize(payload) {
+  return {
     type: FILTER_BY_SIZE,
-    payload
-  }
+    payload,
+  };
 }
-export function filterProductByPrice(payload){
-  return{
+export function filterProductByPrice(payload) {
+  return {
     type: FILTER_BY_PRICE,
-    payload
-  }
+    payload,
+  };
 }
