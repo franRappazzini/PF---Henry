902cab33d4ab10b4d0a1588a35e0a6c0cb337577
agustincoronel1 <agustincoronelb@gmail.com>
2022-08-19 18:51:01 -0300

    feat filtro categories

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -4,19 +4,28 @@ import { useDispatch, useSelector } from "react-redux";
 import Card from "../../organisms/Card/Card";
 import { getAllProducts } from "../../../redux/actions/productActions.js";
 import style from "./Home.module.css";
+import Order from "../../organisms/Order/Order";
+import Filters from "../../organisms/Filters/Filters";
 
 let Home = () => {
-  let { products } = useSelector((state) => state.product);
+  let { filteredProducts } = useSelector((state) => state.product);
   let dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllProducts());
   }, []);
   return (
+    <div>
+
+        <Order />
+        <Filters/>
     <div className={style.container}>
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <Card product={product} />
       ))}
+    </div>
+
+
     </div>
   );
 };
