929845efc306e1b53cd12a6a373346f5ff43e085
agustincoronel1 <agustincoronelb@gmail.com>
2022-08-19 16:09:56 -0300

    feat(organisms\Order\Order) filtro

---
+++ client/src/components/organisms/Filters/Filters.jsx
@@ -0,0 +1,31 @@
+// import React, { useState } from 'react';
+
+// import { Link } from 'react-router-dom';
+
+// import { getAllProducts } from "../../../redux/actions/productActions.js";
+
+// import { filterProductByPrice } from '../../../redux/actions/productActions';
+
+// import style from './Card.module.css';
+
+// function Filters(){
+//     return(
+//         <div>
+            
+// <div>
+// <select  onChange={e=>handleFilterContinent(e)} >
+//         <option value="" disabled selected hidden>BRAND</option>
+//              <option value='All' >All the products</option>
+//              <option value='Nike' >Africa</option>
+//              <option value='Adidas' >South America</option>
+//              <option value='Puma' >Antarctica</option>
+//              <option value='New Balance' >Asia</option>
+//              <option value='Reebook' >Europe</option>
+//         </select>
+// </div>
+
+
+//         </div>
+//     )
+// }
+// export default Filters;
