90576429bf894842f28229e5fa7944789a531d75
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-21 11:49:52 -0300

    fix (server/utils): export function

--- server/utils/functions.js
+++ server/utils/functions.js
@@ -1,5 +1,7 @@
 const { createHash } = require("crypto");
 
-export function hash(pass) {
+function hash(pass) {
   return createHash("sha256").update(pass).digest("hex");
 }
+
+module.exports = { hash };
