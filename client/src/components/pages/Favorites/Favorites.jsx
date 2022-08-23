3dada2583df3f152a0cbf1afe447f0ff214c47a5
Alejo Ufano <alejiiiinn@gmail.com>
2022-08-23 15:37:36 -0300

    Feat(component/Favorites) Carousel added to favs

--- client/src/components/pages/Favorites/Favorites.jsx
+++ client/src/components/pages/Favorites/Favorites.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Card from '../../organisms/Card/Card';
 import style from './Favorites.module.css';
+import Slider from '../../organisms/Carousel/Carousel.jsx';
 
 export default function Favorites() {
   let { favorites } = useSelector((state) => state.product)
 
   return (
-    <div className={style.favoritesContainer}>
-        {
-        favorites.map(favorite => <Card key={favorite.id} product={favorite}/>)
-        }
+<div className={style.globalContainer}>
+
+    <div className={style.carouselContainer}>
+      <Slider/>
     </div>
+    
+    <div className={style.favoritesContainer}>
+      {
+        favorites.map(favorite => <Card key={favorite.id} product={favorite}/>)
+      }
+    </div>
+
+</div>
   )
 }
