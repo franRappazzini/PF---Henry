801ef149ad92bcaf1b756b5bd9291aa063603c98
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-24 20:59:04 -0300

    feat (server/routes): include models in GET /user/:email

--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -1,16 +1,29 @@
-const { user } = require("../../db/db");
+const { user, Rating, Bougth } = require("../../db/db");
 const Router = require("express");
 const userRouter = Router();
 
-userRouter.post("", async (req, res) => {
-  const { name, lastName, email, password, isAdmin } = req.body;
-  const newUser = { name, lastName, email, password, isAdmin };
+// este lo saque porque se crea con auth
+// userRouter.post("", async (req, res) => {
+//   const { name, lastName, email, password, isAdmin } = req.body;
+//   const newUser = { name, lastName, email, password, isAdmin };
+
+//   try {
+//     await user.create(newUser);
+//     res.status(200).json({ success: "User created!" });
+//   } catch (err) {
+//     res.status(400).json({ error: err.message });
+//   }
+// });
+
+userRouter.get("/:email", async (req, res) => {
+  const { email } = req.params;
 
   try {
-    await user.create(newUser);
-    res.status(200).json({ success: "User created!" });
+    const include = [{ model: Bougth }, { model: Rating }];
+    const response = await user.findOne({ where: { email }, include });
+    res.json(response);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(404).json({ error: err.message });
   }
 });
 
