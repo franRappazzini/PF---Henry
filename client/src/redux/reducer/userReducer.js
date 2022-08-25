80701b97d140b6d69f4113bf49d8d98da30fc508
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-24 21:00:02 -0300

    feat (client/redux): actions and reducers for user created

--- client/src/redux/reducer/userReducer.js
+++ client/src/redux/reducer/userReducer.js
@@ -1,7 +1,13 @@
-const initialState = {};
+import { GET_LOGED_USER } from "../../utils/reduxVars";
+
+const initialState = {
+  logedUser: {},
+};
 
 export function userReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_LOGED_USER:
+      return { ...state, logedUser: action.payload };
     default:
       return state;
   }
--- client/src/utils/reduxVars.js
+++ client/src/utils/reduxVars.js
@@ -4,5 +4,7 @@ export const GET_PRODUCT_NAME = "GET_PRODUCT_NAME";
 export const ADD_FAVORITES = "ADD_FAVORITES";
 export const REMOVE_FAVORITES = "REMOVE_FAVORITES";
 export const FILTERS = "FILTERS";
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+export const GET_LOGED_USER = "GET_LOGED_USER";
