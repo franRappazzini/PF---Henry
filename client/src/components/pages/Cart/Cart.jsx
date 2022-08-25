3b953c2e0f59a0524cc294d252613409e0a455f7
Alejo Ufano <alejiiiinn@gmail.com>
2022-08-25 14:21:03 -0300

    Fix(NoProd) Added to fav page message fixes

--- client/src/components/pages/Cart/Cart.jsx
+++ client/src/components/pages/Cart/Cart.jsx
@@ -26,7 +26,7 @@ export default function Cart(){
       )
       }else{
         return(
-          <NoProductsFound message='There are no products added to favorites, im sorry.'/>
+          <NoProductsFound message='There are no products added to cart, im sorry.'/>
         )
       }
 }
