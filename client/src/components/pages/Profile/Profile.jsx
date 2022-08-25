6796f21ae56c958dd3e451ebd4f59342d9a41577
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-24 21:00:54 -0300

    feat (client/Profile): loged user getted

--- client/src/components/pages/Profile/Profile.jsx
+++ client/src/components/pages/Profile/Profile.jsx
@@ -1,12 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Button } from "@mui/material";
 import React from "react";
+import { getLogedUser } from "../../../redux/actions/userActions";
 import style from "./Profile.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 function Profile() {
   const { isAuthenticated, loginWithPopup, user, isLoading } = useAuth0();
+  const { logedUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    isAuthenticated && dispatch(getLogedUser(user.email));
+  }, [dispatch, isAuthenticated, user?.email]);
 
   if (isLoading) return <p>Loading...</p>;
+
+  isAuthenticated && console.log(logedUser);
 
   return (
     <main>
