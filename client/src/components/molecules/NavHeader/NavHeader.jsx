32a482a4429a71eae38f6423822471a9286b8748
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-25 15:59:40 -0300

    feat (client/Header): isAdmin is verified for create prods

--- client/src/components/molecules/NavHeader/NavHeader.jsx
+++ client/src/components/molecules/NavHeader/NavHeader.jsx
@@ -1,15 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { NavLink } from "react-router-dom";
-import React from "react";
+import { getLogedUser } from "../../../redux/actions/userActions";
 import style from "./NavHeader.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function NavHeader() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
+  const { logedUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    isAuthenticated && dispatch(getLogedUser(user.email));
+  }, [dispatch, isAuthenticated, user?.email]);
 
   return (
     <nav className={style.nav}>
       <ul className={style.ul_nav}>
-        {isAuthenticated && (
+        {logedUser?.isAdmin && (
           <li className={style.li_nav}>
             <NavLink
               to={"/create_product"}
@@ -20,7 +29,7 @@ function NavHeader() {
               Create product
             </NavLink>
           </li>
-        )} 
+        )}
       </ul>
     </nav>
   );
