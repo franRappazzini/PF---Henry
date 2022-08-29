32f193d2c6b910322c6d1cceb5d44006e3fcacdb
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-28 21:23:05 -0300

    feat (client/Profile): component updated

---
+++ client/src/components/molecules/ProfileForm/ProfileForm.jsx
@@ -0,0 +1,151 @@
+import { Button, Card, CardContent, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+import { LoadingButton } from "@mui/lab";
+import Swal from "sweetalert2";
+import axios from "axios";
+import style from "./ProfileForm.module.css";
+import { updateUser } from "../../../redux/actions/userActions";
+import withReactContent from "sweetalert2-react-content";
+
+const instanceForm = {
+  picture: "",
+  given_name: "",
+  family_name: "",
+  email: "",
+  password: "",
+};
+
+function ProfileForm({ logedUser, setEdit }) {
+  const [form, setForm] = useState(instanceForm);
+  const [loading, setLoading] = useState(false);
+  const swal = withReactContent(Swal);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (validate()) return swal.fire("Error..", validate(), "error");
+
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("file", form.picture);
+    formData.append("upload_preset", "gsx0rfx1");
+    const imgRes = await axios.post(
+      "https://api.cloudinary.com/v1_1/dnwamkq58/upload",
+      formData
+    );
+
+    if (imgRes.response?.data.error) {
+      setLoading(false);
+      return swal.fire("Error..", imgRes.message, "error");
+    }
+
+    const user = {
+      ...form,
+      picture: imgRes.data.url,
+      currentEmail: logedUser.email,
+    };
+    const res = await updateUser(user);
+    if (res) {
+      setLoading(false);
+      return swal.fire("Error..", res.message, "error");
+    }
+
+    swal.fire("Success!", "User update!", "success");
+    setForm(instanceForm);
+    setLoading(false);
+    setEdit(false);
+  }
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleChangeImage(e) {
+    setForm({ ...form, picture: e.target.files[0] });
+  }
+
+  function validate() {
+    const reEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (form.picture === "") return "Image error";
+    if (form.given_name === "") return "Name error";
+    if (form.family_name === "") return "Last name error";
+    if (!form.email.match(reEmail)) return "Email error";
+  }
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <form className={style.form_profile} onSubmit={handleSubmit}>
+          <TextField
+            type="file"
+            label="Image"
+            variant="standard"
+            autoComplete="off"
+            name="image"
+            onChange={handleChangeImage}
+            // value={form.image}
+            required
+          />
+
+          <TextField
+            type="text"
+            label="Name"
+            variant="standard"
+            autoComplete="off"
+            name="given_name"
+            placeholder={logedUser.given_name}
+            onChange={handleChange}
+            value={form.given_name}
+            required
+          />
+
+          <TextField
+            type="text"
+            label="Last name"
+            variant="standard"
+            autoComplete="off"
+            name="family_name"
+            placeholder={logedUser.family_name}
+            onChange={handleChange}
+            value={form.family_name}
+            required
+          />
+
+          <TextField
+            type="email"
+            label="Email"
+            variant="standard"
+            autoComplete="off"
+            name="email"
+            placeholder={logedUser.email}
+            onChange={handleChange}
+            value={form.email}
+            required
+          />
+
+          {/* <TextField
+          type="password"
+          label="Password"
+          variant="standard"
+          name="password"
+          onChange={handleChange}
+          value={form.password}
+        /> */}
+
+          <div className={style.btns_container}>
+            <LoadingButton loading={loading} type="submit" variant="outlined">
+              Submit
+            </LoadingButton>
+            <Button color="error" size="small" onClick={() => setEdit(false)}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ProfileForm;
--- client/src/components/pages/Profile/Profile.jsx
+++ client/src/components/pages/Profile/Profile.jsx
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Button } from "@mui/material";
-import React from "react";
+import ProfileCard from "../../molecules/ProfileCard/ProfileCard";
+import ProfileForm from "../../molecules/ProfileForm/ProfileForm";
 import { getLogedUser } from "../../../redux/actions/userActions";
 import style from "./Profile.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -10,23 +12,25 @@ import { useEffect } from "react";
 function Profile() {
   const { isAuthenticated, loginWithPopup, user, isLoading } = useAuth0();
   const { logedUser } = useSelector((state) => state.user);
+  const [edit, setEdit] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     isAuthenticated && dispatch(getLogedUser(user));
-  }, [dispatch, isAuthenticated, user]);
+  }, [dispatch, isAuthenticated, user, edit]);
 
   if (isLoading) return <p>Loading...</p>;
 
   isAuthenticated && console.log(logedUser);
 
   return (
-    <main>
-      {isAuthenticated ? (
-        <section className={style.user_data_container}>
-          <img src={user.picture} alt={user.name} />
-          <p>{user.email}</p>
-        </section>
+    <main className={style.profile_container}>
+      {isAuthenticated && logedUser.email ? (
+        !edit ? (
+          <ProfileCard user={user} logedUser={logedUser} setEdit={setEdit} />
+        ) : (
+          <ProfileForm logedUser={logedUser} setEdit={setEdit} />
+        )
       ) : (
         <Button onClick={loginWithPopup}>Log In</Button>
       )}
