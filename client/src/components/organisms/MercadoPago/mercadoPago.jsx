489da0d09a972c58004db4cdb033c92c2d77024b
Franco Pressenda <francopress26@hotmail.com>
2022-08-31 19:26:53 -0300

    feat(server/mercadopago) Andando c item d carrito

---
+++ client/src/components/organisms/MercadoPago/mercadoPago.jsx
@@ -0,0 +1,31 @@
+// import React, { useEffect } from 'react';
+
+// import axios from 'axios'
+
+// export default function mercadoPago(data) {
+
+
+//     useEffect(()=>{
+//         const script = document.createElement('script')
+
+//         const attr_data_preference= document.createAttribute('data-preference-id')
+//         script.setAttributeNode(attr_data_preference)
+//         console.log(data)
+
+
+//         document.getElementById('form1').appendChild(script)
+
+//         return()=>{
+//             document.getElementById('form1').removeChild(script)
+//         }
+//     },[data])
+
+//     return(
+//         <div>
+//             <form id='form1'>
+//                 <h4>Checkout</h4>
+//             </form>
+//         </div>
+
+//     )
+// }
