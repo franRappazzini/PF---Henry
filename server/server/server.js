be48134511bb28158d5a898db4ca8fc4cb1b16e6
Franco Pressenda <francopress26@hotmail.com>
2022-08-31 11:48:52 -0300

    feat(mercadopago) anda la prueba

--- server/server/server.js
+++ server/server/server.js
@@ -7,6 +7,7 @@ const category = require("./routes/category");
 const server = express();
 const product = require("./routes/product");
 const user = require("./routes/user");
+const mercadopago1 = require("./routes/mercadopago");
 
 server.use(cors());
 server.use(express.json());
@@ -15,7 +16,7 @@ server.use("/category", category);
 server.use("/brand", brand);
 server.use("/size", size);
 server.use("/user", user);
-
+server.use("/mercadopago", mercadopago1);
 const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
