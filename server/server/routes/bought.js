3e441657f921b6e4f87d8b5659ffbac2df85780a
Felipe <garciasuezfelipe@gmail.com>
2022-09-01 19:17:27 -0300

    category filter bought

--- server/server/routes/bought.js
+++ server/server/routes/bought.js
@@ -5,7 +5,9 @@ const {
     Product,
     user,
     Product_Size,
-    Bought
+    Bought,
+    Category,
+    Op
 } = require("../../db/db")
 
 //NODEMAILER
@@ -36,10 +38,10 @@ bought.post("", async (req, res) => {
             findProduct_Size.save()
 
         })
-        // const userData = await user.findOne({where: {id: userId}})
+        const userData = await user.findOne({where: {id: userId}})
         await transporter.sendMail({
             from: '"Kemba" hiram.gutkowski@ethereal.email', // sender address
-            to: "fely_kapo@hotmail.com", // list of receivers
+            to: userData, // list of receivers
             subject: "Compra realizada", // Subject line
             text: "HOLAAAAAAAAAAAAAAAAAAAAA", // plain text body
             html: "<b>HOLANDAAAAAAAAA</b>", // html body
@@ -52,6 +54,44 @@ bought.post("", async (req, res) => {
         res.status(400).send(e.message)
     }
 
+})
+
+bought.get("", async (req, res) => {
+    const {categoryId, brandId, user} = req.body
+
+    try {
+        const orders = await Bought.findAll({include: {model: Product_Size}})
+
+            for(let i = 0; i < orders.length; i++) {
+                for(let j = 0; j < orders[i].Product_Sizes.length; j++){
+                    const prod = await Product.findOne({include: {model:Category},where: {id: orders[i].Product_Sizes[j].ProductId}})
+                    
+                    orders[i].Product_Sizes[j].dataValues.productData = prod
+                }
+            }
+        const finalOrders = orders
+        const finalOrdersCat = []
+
+        if(categoryId) {
+            finalOrders.forEach((ord) => {
+                //Recorremos los productos de la compra
+                ord.Product_Sizes.forEach((prod) => {
+                    //Recorremos las categorias de cada producto
+                    prod.dataValues.productData.Categories.forEach((cat) => {
+                        if(cat.id === categoryId) {
+                            finalOrdersCat.push(ord)
+                        }
+                    })
+                })
+            })
+        }
+
+        res.status(200).send(finalOrdersCat)
+    } catch (e) {
+        console.log(e)
+        res.status(400).send("ERROR")
+    }
+    
 })
 
 
