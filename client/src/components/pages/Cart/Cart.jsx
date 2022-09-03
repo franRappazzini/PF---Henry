b4c7a1389fd269ac55168e1949502ecd58a371d9
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-03 12:46:17 -0300

    fix (client): console errors fixed

--- client/src/components/pages/Cart/Cart.jsx
+++ client/src/components/pages/Cart/Cart.jsx
@@ -1,63 +1,59 @@
+import React, { useEffect, useState } from "react";
 
-import React, { useEffect, useState } from 'react';
-import CartCard from '../../organisms/CartCard/CartCard.jsx'
-import style from "./Cart.module.css"
-import { useSelector } from 'react-redux';
-import axios from 'axios'
-import {Link} from 'react-router-dom'
+import { Button } from "@mui/material";
+import CartCard from "../../organisms/CartCard/CartCard.jsx";
+import { Link } from "react-router-dom";
+import NoProductsFound from "../../molecules/NoProductsFound/NoProductsFound.jsx";
+import axios from "axios";
+import mercadopago from "../../organisms/MercadoPago/mercadoPago.jsx";
+import style from "./Cart.module.css";
+import { useSelector } from "react-redux";
 
-import mercadopago from '../../organisms/MercadoPago/mercadoPago.jsx'
-
-import NoProductsFound from '../../molecules/NoProductsFound/NoProductsFound.jsx'
-import { Button } from '@mui/material';
-
-
-export default function Cart(){
+export default function Cart() {
   // const mercadopago = new MercadoPago('1227878501550808', {
   //   locale: 'YOUR_LOCALE' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
   // });
-  let { cartProducts } = useSelector((state) => state.product)
-  
-  let lsCartProducts = JSON.parse(localStorage.getItem('lsCartProducts')) || []
-  const [datos,setDatos]= useState("")
-    useEffect(()=>{
-      axios
-      .post("http://localhost:3001/mercadopago/payment",{lsCartProducts})
-      .then((data)=>{
-        setDatos(data.data)
-        console.log("data:")
-        console.log(data)
+  let { cartProducts } = useSelector((state) => state.product);
+
+  let lsCartProducts = JSON.parse(localStorage.getItem("lsCartProducts")) || [];
+  const [datos, setDatos] = useState("");
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/mercadopago/payment", { lsCartProducts })
+      .then((data) => {
+        setDatos(data.data);
+        console.log("data:");
+        console.log(data);
       })
-      .catch(err=> console.error(err))
-    },[])
-    console.log("lsCartProducts from Cart: ", lsCartProducts)    
-      return (
-        <div className={style.cart_container}>
-          <script src="https://sdk.mercadopago.com/js/v2"></script>
-          <h1 className={style.h1_cart}>MY CART</h1>
-          <div
-            className={
-              cartProducts.length ? style.card_container : style.empty_container
-            }
-          >
-            {lsCartProducts.length ? (
-              lsCartProducts.map((e) => (
-                <div>
-                  <CartCard key={e.id} product={e} />
-                  {/* <span>{e.choosedAmount}</span><span>{e.choosedSize}</span> */}
-                </div>
-              ))
-            ) : (
-              <NoProductsFound message="You haven't added products to the cart... yet ;)" />
-            )}
-          </div>
-          
-              
-         <button className={style.buy_button}><a href={datos} className={style.a}>BUY</a> </button>
-          
-        </div>
-      );
+      .catch((err) => console.error(err));
+  }, []);
+  console.log("lsCartProducts from Cart: ", lsCartProducts);
+  return (
+    <div className={style.cart_container}>
+      <script src="https://sdk.mercadopago.com/js/v2"></script>
+      <h1 className={style.h1_cart}>MY CART</h1>
+      <div
+        className={
+          cartProducts.length ? style.card_container : style.empty_container
+        }
+      >
+        {lsCartProducts.length ? (
+          lsCartProducts.map((e) => (
+            <div key={e.id}>
+              <CartCard product={e} />
+              {/* <span>{e.choosedAmount}</span><span>{e.choosedSize}</span> */}
+            </div>
+          ))
+        ) : (
+          <NoProductsFound message="You haven't added products to the cart... yet ;)" />
+        )}
+      </div>
 
-      
-
+      <button className={style.buy_button}>
+        <a href={datos} className={style.a}>
+          BUY
+        </a>{" "}
+      </button>
+    </div>
+  );
 }
