e1cf4463844bde3494860449433eccaf5f6ea6c1
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2022-09-03 17:31:31 -0300

    feat(redux/RatingActions)adding actions for rating

--- client/src/utils/reduxVars.js
+++ client/src/utils/reduxVars.js
@@ -8,5 +8,6 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const GET_LOGED_USER = "GET_LOGED_USER";
-export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_USERS = 'GET_ALL_USERS'
 export const PUT_PRODUCT = 'PUT_PRODUCT'
+export const DELETE_REVIEW = 'DELETE_REVIEW'
