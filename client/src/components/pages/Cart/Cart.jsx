bcad207b0e75582ff9d2733f0d2995f158f80478
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-04 18:44:42 -0300

    fix (client): update choosedAmount in cart fixed

--- client/src/components/pages/Cart/Cart.jsx
+++ client/src/components/pages/Cart/Cart.jsx
@@ -2,22 +2,15 @@ import React, { useEffect, useState } from "react";
 
 import CartCard from "../../organisms/CartCard/CartCard.jsx";
 import NoProductsFound from "../../molecules/NoProductsFound/NoProductsFound.jsx";
-import { Redirect } from "react-router-dom";
 import axios from "axios";
-import { getLogedUser } from "../../../redux/actions/userActions.js";
-import mercadopago from "../../organisms/MercadoPago/mercadoPago.jsx";
-import { savePurchase } from "../../../redux/actions/otherActions.js";
 import style from "./Cart.module.css";
-import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 //Preguntar si x query llega un status failed y mostrar un toast
 
 export default function Cart() {
   let { cartProducts } = useSelector((state) => state.product);
-  let dispatch = useDispatch();
 
   let lsCartProducts = JSON.parse(localStorage.getItem("lsCartProducts")) || [];
 
@@ -28,16 +21,12 @@ export default function Cart() {
       .post("/mercadopago/payment", { lsCartProducts })
       .then((data) => {
         setDatos(data.data);
-        // console.log("data:")
-        // console.log(data)
       })
       .catch((err) => console.error(err));
   }, []);
 
-  //   useEffect(()=>{
-  //    dispatch(savePurchase(lsCartProducts))
-  // })
   console.log("lsCartProducts from Cart: ", lsCartProducts);
+
   return (
     <div className={style.cart_container}>
       {/* <script src="https://sdk.mercadopago.com/js/v2"></script> */}
