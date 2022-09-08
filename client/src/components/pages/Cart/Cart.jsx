b1673e572088f1050129b2fa783eb912f42f82f5
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-08 13:01:57 -0300

    fix

--- client/src/components/pages/Cart/Cart.jsx
+++ client/src/components/pages/Cart/Cart.jsx
@@ -92,7 +92,7 @@ export default function Cart(product) {
     // const imageRegex = /^.+.(jpe?g|gif|png)$/i;
     if (user === undefined) return "Only loged user can buy in this page!";
     if (logedUser.isAdmin) return "Admins can´t buy in this page";
-    if (!user.email_verified) return "You must verify your email!";
+    if (!logedUser.email_verified) return "You must verify your email!";
   }
 
   const onClickBuy = () => {
