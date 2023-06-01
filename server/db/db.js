d6e4420a90fb50c4e9297d69cad9e1077c069845
Francisco Rappazzini <81725268+franRappazzini@users.noreply.github.com>
2023-06-01 16:48:39 -0300

    refactor - update sequelize connection

--- server/db/db.js
+++ server/db/db.js
@@ -10,36 +10,45 @@ const path = require("path");
 // );
 
 // para poder deployear
-const sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: { max: 3, min: 1, idle: 10000 },
-        dialectOptions: {
-          ssl: { require: true, rejectUnauthorized: false },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        // "postgres://ppahdjqtzybfaj:5ce7f5f093976f97cdf0853ca284237ded3ef0b6fd9d7919b01a2aefc55dc72a@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d1h8d8a1rn576h", // heroku
-        "postgres://iwukxuix:xou9rxFqT6Web2ZTlntBSzdoP2Rixd7O@suleiman.db.elephantsql.com/iwukxuix  ", // elephant
+// const sequelize =
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize({
+//         database: DB_NAME,
+//         dialect: "postgres",
+//         host: DB_HOST,
+//         port: 5432,
+//         username: DB_USER,
+//         password: DB_PASSWORD,
+//         pool: { max: 3, min: 1, idle: 10000 },
+//         dialectOptions: {
+//           ssl: { require: true, rejectUnauthorized: false },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         // "postgres://ppahdjqtzybfaj:5ce7f5f093976f97cdf0853ca284237ded3ef0b6fd9d7919b01a2aefc55dc72a@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d1h8d8a1rn576h", // heroku
+//         "postgres://iwukxuix:xou9rxFqT6Web2ZTlntBSzdoP2Rixd7O@suleiman.db.elephantsql.com/iwukxuix  ", // elephant
 
-        // "postgres://nxzqhyvx:G0f2IN2Wj_KgM18-rpELCYQ4zkV164ZJ@kesavan.db.elephantsql.com/nxzqhyvx", //felipe
-        // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//         // "postgres://nxzqhyvx:G0f2IN2Wj_KgM18-rpELCYQ4zkV164ZJ@kesavan.db.elephantsql.com/nxzqhyvx", //felipe
+//         // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 
-        // "postgres://hqylrscg:CjiddTbf3QlMiuaNMJVmQcy_uJvB0yVM@suleiman.db.elephantsql.com/hqylrscg", //Franco
-        {
-          logging: false,
-          // dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, // config para heroku
-        }
-      );
+//         // "postgres://hqylrscg:CjiddTbf3QlMiuaNMJVmQcy_uJvB0yVM@suleiman.db.elephantsql.com/hqylrscg", //Franco
+//         {
+//           logging: false,
+//           // dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, // config para heroku
+//         }
+//       );
 
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
