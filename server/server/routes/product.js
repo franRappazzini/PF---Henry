9837e4d755b4821f67198e6dea61eb69ca00cab7
Francisco Rappazzini <81725268+franRappazzini@users.noreply.github.com>
2023-06-01 15:36:35 -0300

    fix - solved

--- server/server/routes/product.js
+++ server/server/routes/product.js
@@ -1,33 +1,25 @@
-const {Router} = requier('express')
-const router=Router();
-const{Product}=require("../../db/models/Product")
+const { Router } = require("express");
+const router = Router();
+const { Product } = require("../../db/models/Product");
 
+router.get("/", async (req, res) => {
+  try {
+    const allProducts = await Product.findAll({
+      include: {
+        // model: Category,
+        // attributes: {
+        //   include: ['name'],
+        //   exclude:['createdAt', 'updatedAt']
+        // },
+        // through: {
+        //   attributes:[]
+        // }
+      },
+    });
+    res.status(200).send(allProducts);
+  } catch (error) {
+    res.status(400).send("Problem");
+  }
+});
 
-
-router.get('/',async (req,res)=>{
-    try {
-        const allProducts=await Product.findAll({
-            include:{   
-                // model: Category,
-                // attributes: {
-                //   include: ['name'], 
-                //   exclude:['createdAt', 'updatedAt']
-                // },
-                // through: {
-                //   attributes:[]
-                // }
-            }
-        })
-        res.status(200).send(allProducts)
-    } catch (error) {
-        res.status(400).send("Problem")
-    }
-   
-
-    
-})
-
-router.post('/',async(req,res)=>{
-
-})
-
+router.post("/", async (req, res) => {});
